d25367d69cd71427dfdf70ab3878bb23cecb552b
SuJiaKuan <jk.su@ingenta.ai>
2021-03-20 23:28:04 +0800

    Support databse saving for assignment submission API

--- src/models.py
+++ src/models.py
@@ -2,3 +2,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
+
+
+class Assignment(db.Model):
+    __tablename__ = "assignments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    sid = db.Column(db.String(), nullable=False)
+    ordinal = db.Column(db.Integer, nullable=False)
+    correctnesses = db.Column(db.String(), nullable=False)
--- src/resources.py
+++ src/resources.py
@@ -3,9 +3,29 @@ from flask_restful import Resource
 
 from errors import ParameterMissing
 from assignments import assignment_function_mapping
+from models import db
+from models import Assignment
 
 
 class AssignmentSubmission(Resource):
+
+    def _serialize_correctnesses(self, correctnesses):
+        return ",".join([
+            "1" if c else "0"
+            for c in correctnesses
+        ])
+
+    def _deserialize_correctnesses(self, correctnesses):
+        return [
+            True if c == "1" else False
+            for c in correctnesses.split(",")
+        ]
+
+    def _merge_correctnesses(self, old_correctnesses, new_correctnesses):
+        return [
+            o or n
+            for o, n in zip(old_correctnesses, new_correctnesses)
+        ]
 
     def post(self):
         parser = reqparse.RequestParser()
@@ -32,6 +52,29 @@ class AssignmentSubmission(Resource):
             raise ParameterMissing
 
         correctnesses = assignment_function_mapping[args.ordinal](args.answers)
+
+        past_assignment = Assignment.query.filter_by(
+            sid=args.sid,
+            ordinal=args.ordinal,
+        ).first()
+
+        if past_assignment is not None:
+            updated_correctenesses = self._merge_correctnesses(
+                self._deserialize_correctnesses(past_assignment.correctnesses),
+                correctnesses,
+            )
+            past_assignment.correctnesses = self._serialize_correctnesses(
+                updated_correctenesses,
+            )
+            db.session.commit()
+        else:
+            assignment = Assignment(
+                sid=args.sid,
+                ordinal=args.ordinal,
+                correctnesses=self._serialize_correctnesses(correctnesses),
+            )
+            db.session.add(assignment)
+            db.session.commit()
 
         return {
             "correctnesses": correctnesses,
